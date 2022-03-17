c48cffb70f6010dff4e9d613e4250745b6054a9c
thxtome <thxtome531@gmail.com>
2022-03-17 23:30:54 +0900

    init project
    start project
    
    프로젝트 시작!

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Jpark의 Portfolio</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
