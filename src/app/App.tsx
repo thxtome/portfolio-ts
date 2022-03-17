54bf8d8b636159fee156ece87af33b159e1c575f
thxtome <thxtome531@gmail.com>
2022-03-17 23:50:05 +0900

    add font
    add font
    
    폰트 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,44 @@
+//asset
+import NanumSquareBTtf from "./asset/fonts/NanumSquareB.ttf";
+import NanumSquareBWoff from "./asset/fonts/NanumSquareB.woff";
+
+import NanumSquareEBTtf from "./asset/fonts/NanumSquareEB.ttf";
+import NanumSquareEBWoff from "./asset/fonts/NanumSquareEB.woff";
+
+import NanumSquareRTtf from "./asset/fonts/NanumSquareR.ttf";
+import NanumSquareRWoff from "./asset/fonts/NanumSquareR.woff";
+
+const style = document.createElement("style");
+style.textContent = `
+@font-face {
+  font-family: NanumSquare;
+  font-weight: 800;
+  src: url("${NanumSquareEBWoff}") format("woff"), 
+       url("${NanumSquareEBTtf}") format("truetype");
+  letter-spacing: -0.22px;
+}
+
+@font-face {
+  font-family: NanumSquare;
+  font-weight: 600;
+  src: url("${NanumSquareBWoff}") format("woff"),
+       url("${NanumSquareBTtf}") format("truetype");
+  letter-spacing: -0.22px;
+}
+
+@font-face {
+  font-family: NanumSquare;
+  font-weight: 400;
+  src: url("${NanumSquareRWoff}") format("woff"), 
+       url("${NanumSquareRTtf}") format("truetype");
+  letter-spacing: -0.25px;
+}`;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Jpark의 Portfolio</p>
+        <p style={{ fontFamily: "NanumSquare" }}>Jpark의 Portfolio</p>
       </header>
     </div>
   );
